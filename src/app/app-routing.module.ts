29d9dedfa35a70ad3edb01f6bbe696c14f47a101
Jorge Vergara <javebratt@gmail.com>
2018-08-12 17:17:14 -0500

    Adds the AuthGuard to the routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from './services/auth.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  {
+    path: 'home',
+    loadChildren: './home/home.module#HomePageModule',
+    canActivate: [AuthGuard],
+  },
   { path: 'landing', loadChildren: './pages/landing/landing.module#LandingPageModule' },
   {
     path: 'bill-create',
     loadChildren: './pages/bill-create/bill-create.module#BillCreatePageModule',
+    canActivate: [AuthGuard],
   },
   {
     path: 'bill-detail/:id',
     loadChildren: './pages/bill-detail/bill-detail.module#BillDetailPageModule',
+    canActivate: [AuthGuard],
   },
   { path: 'login', loadChildren: './pages/login/login.module#LoginPageModule' },
   {
