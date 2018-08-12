eda5c91ae50f643de30bb4a1152324e81f3bd3f8
Jorge Vergara <javebratt@gmail.com>
2018-08-12 17:41:28 -0500

    Adds the landing page

--- src/app/pages/landing/landing.page.ts
+++ src/app/pages/landing/landing.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { LoadingController } from '@ionic/angular';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-landing',
@@ -6,10 +9,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./landing.page.scss'],
 })
 export class LandingPage implements OnInit {
+  constructor(
+    private loadingCtrl: LoadingController,
+    private authService: AuthService,
+    private router: Router
+  ) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  async goToBillList(): Promise<void> {
+    const loading = await this.loadingCtrl.create();
+    try {
+      loading.present();
+
+      this.authService.anonymousLogin().then(() => {
+        loading.dismiss().then(() => {
+          this.router.navigateByUrl('/home');
+        });
+      });
+    } catch (error) {
+      loading.dismiss().then(() => {
+        console.error(error);
+      });
+    }
   }
-
 }
