cd0f2157b8ea609e5400f892c871bbff574981f8
Jorge Vergara <javebratt@gmail.com>
2018-08-12 19:18:47 -0500

    Adds the bill lists to the home page

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,63 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActionSheetController } from '@ionic/angular';
+import { Router } from '@angular/router';
+import { BillService } from '../services/bill.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  public billList: Observable<any>;
 
+  constructor(
+    private actionSheetCtrl: ActionSheetController,
+    private billService: BillService,
+    private router: Router
+  ) {}
+
+  ngOnInit() {
+    this.billList = this.billService.getBillList().valueChanges();
+  }
+
+  async moreBillOptions(billId): Promise<void> {
+    const action = await this.actionSheetCtrl.create({
+      header: 'Modify your bill',
+      buttons: [
+        {
+          text: 'Delete',
+          role: 'destructive',
+          icon: 'trash',
+          handler: () => {
+            this.billService.removeBill(billId);
+          },
+        },
+        {
+          text: 'More details',
+          icon: 'play',
+          handler: () => {
+            this.router.navigate(['/bill-detail', billId]);
+          },
+        },
+        {
+          text: 'Mark as Paid!',
+          icon: 'checkmark',
+          handler: () => {
+            this.billService.payBill(billId);
+          },
+        },
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          icon: 'close',
+          handler: () => {
+            console.log('Cancel clicked');
+          },
+        },
+      ],
+    });
+    action.present();
+  }
 }
--- src/app/services/bill.service.ts
+++ src/app/services/bill.service.ts
@@ -1,9 +1,56 @@
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from 'angularfire2/auth';
+import {
+  AngularFireDatabase,
+  AngularFireObject,
+  AngularFireList,
+} from 'angularfire2/database';
+import * as firebase from 'firebase/app';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class BillService {
+  public billList: AngularFireList<any>;
+  public userId: string;
 
-  constructor() { }
+  constructor(private afAuth: AngularFireAuth, private afDatabase: AngularFireDatabase) {
+    this.afAuth.authState.subscribe(user => {
+      this.userId = user.uid;
+      this.billList = this.afDatabase.list(`/userProfile/${user.uid}/billList`);
+    });
+  }
+
+  getBillList(): AngularFireList<any> {
+    return this.billList;
+  }
+
+  getBill(billId: string): AngularFireObject<any> {
+    return this.afDatabase.object(`/userProfile/${this.userId}/billList/${billId}`);
+  }
+
+  createBill(
+    name: string,
+    amount: number,
+    dueDate: Date = null,
+    paid: boolean = false
+  ): Promise<any> {
+    const newBillRef: firebase.database.ThenableReference = this.billList.push({});
+
+    return newBillRef.set({
+      name,
+      amount,
+      dueDate,
+      paid,
+      id: newBillRef.key,
+    });
+  }
+
+  removeBill(billId: string): Promise<any> {
+    return this.billList.remove(billId);
+  }
+
+  payBill(billId: string): Promise<any> {
+    return this.billList.update(billId, { paid: true });
+  }
 }
