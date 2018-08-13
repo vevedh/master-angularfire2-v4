aab40486d5d720ba211e15a0a0209199c87bb240
Jorge Vergara <javebratt@gmail.com>
2018-08-12 20:21:59 -0500

    Adds the bill-detail page

--- src/app/pages/bill-detail/bill-detail.page.ts
+++ src/app/pages/bill-detail/bill-detail.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { ActionSheetController, AlertController } from '@ionic/angular';
+import { BillService } from '../../services/bill.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-bill-detail',
@@ -6,10 +10,53 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./bill-detail.page.scss'],
 })
 export class BillDetailPage implements OnInit {
+  public bill: Observable<any>;
+  public billId: string;
 
-  constructor() { }
+  constructor(
+    public router: Router,
+    public route: ActivatedRoute,
+    public actionCtrl: ActionSheetController,
+    public alertCtrl: AlertController,
+    public billService: BillService
+  ) {}
 
   ngOnInit() {
+    this.billId = this.route.snapshot.paramMap.get('id');
+    this.bill = this.billService.getBill(this.billId).valueChanges();
   }
 
+  async showOptions(): Promise<void> {
+    const action = await this.actionCtrl.create({
+      header: 'Modify your bill',
+      buttons: [
+        {
+          text: 'Delete',
+          role: 'destructive',
+          icon: 'trash',
+          handler: () => {
+            this.billService.removeBill(this.billId).then(() => {
+              this.router.navigateByUrl('home');
+            });
+          },
+        },
+        {
+          text: 'Mark as Paid!',
+          icon: 'checkmark',
+          handler: () => {
+            this.billService.payBill(this.billId);
+          },
+        },
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          icon: 'close',
+          handler: () => {
+            console.log('Cancel clicked');
+          },
+        },
+      ],
+    });
+    action.present();
+  }
 }
